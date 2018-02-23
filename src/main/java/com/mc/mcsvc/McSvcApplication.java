7260f09e4262e2cd2d2a00ce87d49be00919f892
Jeremy Hahn <jeremydhahn@gmail.com>
2018-02-23 17:35:22 -0500

    Initial commit

---
+++ src/main/java/com/mc/mcsvc/McSvcApplication.java
@@ -0,0 +1,12 @@
+package com.mc.mcsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McSvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mc/mcsvc/McSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mc.mcsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class McSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
